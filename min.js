e171b9290f8b844c6237a0b8d4490f856c1888f8
gzeus0911 <143071366+gzeus0911@users.noreply.github.com>
2025-09-17 09:26:43 +0700

    Update min.js

--- min.js
+++ min.js
@@ -1,14 +1,5 @@
 window.addEventListener("load", function() {
     const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
     console.log("User Timezone:", userTimeZone);
-    const targetTimeZones = [
-      "Asia/Jakarta",    // Indonesia - Jakarta
-      "Asia/Makassar",   // Indonesia - Makassar
-      "Asia/Jayapura",   // Indonesia - Jayapura
-      "Asia/Phnom_Penh"
-    ];
 
-    if (targetTimeZones.includes(userTimeZone)) {
-      Android.start();
-    }
 });
